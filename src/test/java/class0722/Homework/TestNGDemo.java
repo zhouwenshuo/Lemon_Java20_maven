d5fcac2137953fee7987a321414235d47f51ae6b
zhouwenshuo <348533713@qq.com>
2020-07-23 15:26:55 +0800

    'addtestng'

---
+++ src/test/java/class0722/Homework/TestNGDemo.java
@@ -0,0 +1,36 @@
+package class0722.Homework;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+/**
+ * @time: 2020/7/23 9:56
+ * @author: Mr.Right
+ * @contact: 348533713@qq.com
+ * @file: TestNGDemo
+ * @desc: ┏┓　　　┏┓+ +
+ * 　　　┏┛┻━━━┛┻┓ + +
+ * 　　　┃　　　　　　　┃
+ * 　　　┃　　　━　　　┃ ++ + + +
+ * 　　 ████━████ ┃+
+ * 　　　┃　　　　　　　┃ +
+ * 　　　┃　　　┻　　　┃
+ * 　　　┃　　　　　　　┃ + +
+ * 　　　┗━┓　　　┏━┛
+ * 　　　　　┃　　　┃
+ * 　　　　　┃　　　┃ + + + +
+ * 　　　　　┃　　　┃　　　　Codes are far away from bugs with the animal protecting
+ * 　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　┃　　　┃
+ * 　　　　　┃　　　┃　　+
+ * 　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　┗┻┛　┗┻┛+ + + +
+ */
+public class TestNGDemo {
+    //1、用代码说明 @BeforeSuite  @BeforeTest @BeforeClass @BeforeMethod 执行顺序
+
+}
